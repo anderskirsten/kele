14209027fe9f741929a96c865f40a00b10a7c99b
Kirsten Anderson <anders.kirsten@gmail.com>
2017-09-29 23:49:47 +0000

    Retrieve User Function: Complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,19 +1,25 @@
 require 'httparty'
+require 'json'
 require './lib/kele'
 
 class Kele
     include HTTParty
 
     def initialize(email, password)
-        response = self.class.post((api_base_url + '/sessions'), body: {email: email, password: password})
+        response = self.class.post(api_url("sessions"), body: {email: email, password: password})
         raise "Invalid email or password" if response.code == 404
         @auth_token = response['auth_token']
     end
     
+    def get_me
+        response = self.class.get(api_url("users/me"), headers: {authorization: @auth_token})
+        @user_data = (JSON.parse(response.body)).to_h
+    end
+    
     private
     
-    def api_base_url
-       'https://www.bloc.io/api/v1'
+    def api_url(end_point)
+       "https://www.bloc.io/api/v1/#{end_point}"
     end
 
 end
