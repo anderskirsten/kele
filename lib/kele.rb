c3bc666bb634b0582294f65a61ed5a8420979de4
Kirsten Anderson <anders.kirsten@gmail.com>
2017-09-30 17:45:58 +0000

    Mentor Availability Retrieval Feature: Complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -13,7 +13,12 @@ class Kele
     
     def get_me
         response = self.class.get(api_url("users/me"), headers: {authorization: @auth_token})
-        @user_data = (JSON.parse(response.body)).to_h
+        @user_data = JSON.parse(response.body, {:symbolize_names => true})
+    end
+    
+    def get_mentor_availability(mentor_id)
+        response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"), headers: {authorization: @auth_token})
+        @mentor_availability = JSON.parse(response.body, {:symbolize_names => true})
     end
     
     private
